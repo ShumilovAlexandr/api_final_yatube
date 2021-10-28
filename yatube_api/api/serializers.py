a87c38d83a90fa78db1c05b0f014a2f82115b6c0
ShumilovAlexandr <33032109+ShumilovAlexandr@users.noreply.github.com>
2021-10-29 01:21:34 +0300

    Тесты

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,6 +1,7 @@
 from posts.models import Comment, Follow, Group, Post, User
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -28,19 +29,7 @@ class CommentCreateSerializer(serializers.ModelSerializer):
         fields = ('text',)
 
 
-class FollowCreateSerializer(serializers.ModelSerializer):
-    following = serializers.SlugRelatedField(
-        read_only=False,
-        slug_field='username',
-        queryset=User.objects.all(),
-    )
-
-    class Meta:
-        fields = ('following',)
-        model = Follow
-
-
-class FollowListSerializer(serializers.ModelSerializer):
+class FollowSerializer(serializers.ModelSerializer):
     user = serializers.SlugRelatedField(
         read_only=True,
         slug_field='username',
@@ -53,8 +42,21 @@ class FollowListSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = ('user', 'following')
         model = Follow
+        fields = ('user', 'following',)
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following')
+            )
+        ]
+
+    def validate(self, data):
+        if data['following'] == self.context['request'].user:
+            raise serializers.ValidationError(
+                'following yourself is forbidden!'
+            )
+        return data
 
 
 class GroupSerializer(serializers.ModelSerializer):
--- yatube_api/posts/views.py
+++ yatube_api/posts/views.py
@@ -1,12 +1,12 @@
 from api.permissions import IsAuthorOrReadOnly
-from api.serializers import (CommentSerializer, FollowCreateSerializer,
-                             FollowListSerializer, GroupSerializer,
-                             PostSerializer)
+from api.serializers import (CommentSerializer, FollowSerializer,
+                             GroupSerializer, PostSerializer)
 from rest_framework import mixins, permissions, viewsets, filters
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
 
-from .models import Comment, Follow, Group, Post
+from .models import Comment, Follow, Group, Post, User
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -32,20 +32,15 @@ class CommentViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 
 
-class FollowViewSet(viewsets.ModelViewSet):
-
-    permission_class = (permissions.IsAuthenticated, )
+class FollowViewSet(viewsets.GenericViewSet, CreateModelMixin, ListModelMixin):
+    serializer_class = FollowSerializer
+    permission_classes = (permissions.IsAuthenticated, )
     queryset = Follow.objects.all()
-    filter_backends = (filters.SearchFilter, )
+    filter_backends = (filters.SearchFilter,)
     search_fields = ('user__username', 'following__username')
 
-    def get_serializer_class(self):
-        if self.request.method == 'GET':
-            return FollowListSerializer
-        return FollowCreateSerializer
-
     def get_queryset(self):
-        user = self.request.user
+        user = get_object_or_404(User, username=self.request.user.username)
         return user.follower.all()
 
     def perform_create(self, serializer):
